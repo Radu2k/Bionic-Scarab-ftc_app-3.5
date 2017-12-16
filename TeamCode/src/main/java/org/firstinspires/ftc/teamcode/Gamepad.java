f29a56b0448d01cdb5defeb3b53c34ebde853cf0
flavmodan <flavmodan@gmail.com>
2017-12-16 21:51:47 +0200

    commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gamepad.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gamepad.java
@@ -56,29 +56,16 @@ public class Gamepad extends LinearOpMode {
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftDrive = null;
-    private DcMotor rightDrive = null;
-    private DcMotor upDrive = null;
-    private double upStep=0.2;
 
     @Override
 
     public void runOpMode() {
+
+        controls run = new controls();
+
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
-        upDrive = hardwareMap.get(DcMotor.class,"up_drive");
-
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
-        leftDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);
-        upDrive.setDirection(DcMotor.Direction.FORWARD);
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -100,29 +87,21 @@ public class Gamepad extends LinearOpMode {
             double drive = -gamepad1.left_stick_y*0.75;
             double turn  =  gamepad1.right_stick_x*0.75;
 
+            run.navigate(drive,turn);
+
             if(gamepad1.dpad_up){
-                upLift=upStep;
+                run.lifter_up();
             }else if(gamepad1.dpad_down){
-                upLift=-upStep;
+                run.lifter_down();
             }else{
-                upLift=0.0;
+                run.lifter_stop();
             }
-            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
 
-            // Tank Mode uses one stick to control each wheel.
-            // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            // leftPower  = -gamepad1.left_stick_y ;
-            // rightPower = -gamepad1.right_stick_y ;
 
-            // Send calculated power to wheels
-            leftDrive.setPower(leftPower);
-            rightDrive.setPower(rightPower);
-            upDrive.setPower(upLift);
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+            //telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
             telemetry.update();
         }
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controls.java
@@ -12,22 +12,38 @@ import com.qualcomm.robotcore.util.Range;
  * Created by dave on 12/16/17.
  */
 
-public abstract class controls extends LinearOpMode{
+public class controls extends LinearOpMode{
     //dclaring motors
     private DcMotor leftDrive = null;
     private DcMotor rightDrive = null;
     private DcMotor upDrive = null;
 
+    //declaring tunning variables
+    private double upStep=0.1; //how fast to lift the cube
+    private double leftPower;
+    private double rightPower;
+    private double liftPower=0;
+    private double powerRatio=99.0; //acceleration value the closer to 100 the faster the acceleration
+
     //main navigation function takes in drive as acceleration forward or backward and turn witch controls steering
-    public void navigate(int drive,int turn){
-        double leftPower = Range.clip(drive + turn, -1.0, 1.0) ;
-        double rightPower = Range.clip(drive - turn, -1.0, 1.0);
+    public void navigate(double drive,double turn){
+        leftPower = (powerRatio*Range.clip(drive + turn, -1.0, 1.0)+(100.0-powerRatio)*leftPower)/100.0 ;
+        rightPower = (powerRatio*Range.clip(drive - turn, -1.0, 1.0)+(100.0-powerRatio)*rightPower)/100.0;
         leftDrive.setPower(leftPower);
         rightDrive.setPower(rightPower);
     }
-
+    //
     public void lifter_up(){
 
+        upDrive.setPower(upStep);
+    }
+
+    public void lifter_down(){
+        upDrive.setPower(-upStep);
+    }
+
+    public void lifter_stop(){
+        upDrive.setPower(0);
     }
 
     //main setup phase required to set up motors
@@ -38,5 +54,8 @@ public abstract class controls extends LinearOpMode{
         leftDrive.setDirection(DcMotor.Direction.FORWARD);
         rightDrive.setDirection(DcMotor.Direction.REVERSE);
         upDrive.setDirection(DcMotor.Direction.FORWARD);
+    }
+    controls(){
+        runOpMode();
     }
 }
